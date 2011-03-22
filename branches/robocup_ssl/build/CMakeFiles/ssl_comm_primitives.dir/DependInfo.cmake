3e57dff5d1f569e6ec73be0e3fadb1dc2adf9365
kadirfu <kadirfu@1d6e3ae0-d6b1-11de-b661-09e03a0d8f92>
2011-03-22 23:52:32 +0000

    communication primitives

---
+++ branches/robocup_ssl/build/CMakeFiles/ssl_comm_primitives.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kadir/interconnection/workspace/robocup/system_design/robocup_ssl/ssl_comm_primitives/src/SSLGlobalStateSubscriber.cpp" "/home/kadir/interconnection/workspace/robocup/system_design/robocup_ssl/ssl_comm_primitives/build/CMakeFiles/ssl_comm_primitives.dir/src/SSLGlobalStateSubscriber.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ branches/robocup_ssl/build/CMakeFiles/ssl_comm_primitives.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ssl_comm_primitives.dir/src/SSLGlobalStateSubscriber.o"
+  "../lib/libssl_comm_primitives.pdb"
+  "../lib/libssl_comm_primitives.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ssl_comm_primitives.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
